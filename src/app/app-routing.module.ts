7a98aac2c849010ccc3279ae4efb29c5af37f41f
Sam Radford <smr1292@yahoo.com>
2019-05-16 16:37:00 -0400

    display table of open tickets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectComponent } from './project/project.component';
 import { TicketComponent } from './ticket/ticket.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'project', component: ProjectComponent },
-    { path: 'ticket/:id', component: TicketComponent }
+    { path: 'ticket/:id', component: TicketComponent },
+    { path: 'tickets', component: TicketsComponent }
 ];
 
 @NgModule({
